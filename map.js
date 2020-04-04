dedcee6fb9b9deff2cd5f23da03b55ced7360716
Colm Doyle <collydoyle_88@hotmail.com>
2020-04-04 20:03:58 +0000

    Add info window to markers(needs alteration to show individual info) checked html/js/css format

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-        function mapView() {                                                        // Map in view toggle on and off.
+function mapView() {                                                        // Map in view toggle on and off.
   var mapView = document.getElementById("map");
   if (mapView.style.display === "none") {
     mapView.style.display = "block";
@@ -24,11 +24,12 @@ function success(position) {
 
                 map = new google.maps.Map(document.getElementById("map"), mapOptions);  // Create the map
 
-                var marker = new google.maps.Marker({                               // Place the initial marker
+                homeMarker =  new google.maps.Marker({                               // Place the initial marker
                     position: userLatLng,
                     map: map,
                     title: "Your current location!"
                 });
+
                 var request = {
     location: userLatLng,
     radius: '50',
@@ -37,24 +38,26 @@ function success(position) {
 
     service = new google.maps.places.PlacesService(map);
   service.textSearch(request, callback);
-};
+}
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < 3; i++) {
-      var place = results[i];
+       var place = results[i];
       console.log(place);
-      addMarker(place.geometry.location);
-    }
-  }
-}
-
-function addMarker(position) {
-
-    new google.maps.Marker({
-        position: position,
+      console.log(results[i].name);
+     marker = new google.maps.Marker({
+        position: place.geometry.location,
         map: map
     });
+    var infowindow = new google.maps.InfoWindow
+      ({content: "<b>" + place.name + "</b>" + "<br>" + "<b>Address: </b>" + place.formatted_address + "<br>" + "<b>Rating: </b>" + place.rating});
+    }
+    google.maps.event.addListener(marker, 'click', function() {
+  infowindow.open(map,this);
+});
+    }
+  
+}
+}
 
- }
-};
--- wheelSpin.js
+++ wheelSpin.js
@@ -17,7 +17,7 @@ function spinToggle(){                                                        //
             winningSegment = theWheel.getIndicatedSegment();                // Get the text of the winning segment.
             var winningSegmentNumber = theWheel.getIndicatedSegmentNumber();    // Get the number of the winning segment.
  
-            for (let i = 1; i < theWheel.segments.length; i ++) 
+            for (var i = 1; i < theWheel.segments.length; i ++) 
             {
                 theWheel.segments[i].fillStyle = 'grey';                     // For loop and change fillStyle of all segments to grey.
             }
@@ -32,62 +32,6 @@ function spinToggle(){                                                        //
             console.log(typeof winningSegment);
            
         }
-
-
-
-
-//             function initGeolocation() {
-//                 var request = {
-//     location: userLatLng,
-//     radius: '50',
-//     query: 'restaurant'
-//   };
-
-//   service = new google.maps.places.PlacesService(map);
-//   service.textSearch(request, callback);
-// }
-
-// function callback(results, status) {
-//   if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       var place = results[i];
-//       createMarker(results[i]);
-//     }
-//   }
-// }
-//                 if (navigator.geolocation) {
-//                     navigator.geolocation.getCurrentPosition(success);                // Call getCurrentPosition
-//                 } else {
-//                     alert("Sorry, your browser does not support geolocation services.");
-//                 }
-            
-//             var map;
-//             var service;
-// var infowindow;
-// var request;
-// var markers = [];
-
-//             function success(position) {
-//                 var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); // Define the coordinates
-
-//                 var mapOptions = {
-//                     zoom: 13,
-//                     center: userLatLng,
-//                 };
-
-//                 map = new google.maps.Map(document.getElementById("map"), mapOptions);  // Create the map
-
-//                 var marker = new google.maps.Marker({                               // Place the initial marker
-//                     position: userLatLng,
-//                     map: map,
-//                     title: "Your current location!"
-//                 });
-//                 var marker = new google.maps.Marker({                               // Place the initial marker
-//                     position: {lat: 53.506170, lng: -6.456338},
-//                     map: map,
-//                     title: "coordinates 2!"
-//                 });
-//             }
 
 function reset() {                                                        // Reset Button toggle on and off.
   var reset = document.getElementById("resetButton");
@@ -114,5 +58,5 @@ function reset() {                                                        // Res
             mapView();                      // Toggle mapView function to reset map to hidden.
             spinToggle();                    // Toggle Spin button to On.
             reset();                        // Toggle Reset button to Off.
-            };
+            }
         
